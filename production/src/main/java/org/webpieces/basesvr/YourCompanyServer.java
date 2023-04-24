5ae67ae81e159f770220c0ab5c8204f61fb70b40
Dean Hiller <dean.hiller@tray.com>
2023-04-24 13:16:29 +0200

    upgrade to 2.1.64

--- production/src/main/java/org/webpieces/basesvr/YourCompanyServer.java
+++ production/src/main/java/org/webpieces/basesvr/YourCompanyServer.java
@@ -54,7 +54,7 @@ public abstract class YourCompanyServer {
 			ServerConfig svrConfig, 
 			String ... args
 	) {
-		String base64Key = "BrpR8WuWY18Lb8KGrzhJK9U5emxRP8wv50JtlYqmz8+wIQy19zB+OzHtYMq9gHEcYqC7uuUrmH1nOEHrBjsWdg==";  //This gets replaced with a unique key each generated project which you need to keep or replace with your own!!!		
+		String base64Key = "ym0R3XequLJYhym1kML6fQUa61ZWPvAhPlhrMxdJyv5JPwvVMfMrc4yC/HQEJeTK3LHPAZT0jaeMa/P8pMIcWA==";  //This gets replaced with a unique key each generated project which you need to keep or replace with your own!!!		
 		
 		log.info("Constructing WebpiecesServer with args="+Arrays.asList(args));
 
